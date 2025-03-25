1878dafe3fdbc1a8e80edcc15d6b110676578366
Kishor Mainali <kishor.mainali@ebpearls.com>
2025-03-25 08:57:50 +0545

    chore: upgrade freezed to latest version

--- flutter_google_places_sdk_platform_interface/lib/src/types/fetch_photo_response.dart
+++ flutter_google_places_sdk_platform_interface/lib/src/types/fetch_photo_response.dart
@@ -5,7 +5,7 @@ part 'fetch_photo_response.freezed.dart';
 
 /// The response for a [FlutterGooglePlacesSdkPlatform.fetchPlacePhoto] request
 @freezed
-class FetchPlacePhotoResponse with _$FetchPlacePhotoResponse {
+sealed class FetchPlacePhotoResponse with _$FetchPlacePhotoResponse {
   /// Construct a photo response with image
   const factory FetchPlacePhotoResponse.image(Image image) =
       FetchPlacePhotoResponseImage;
